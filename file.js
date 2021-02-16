eeeb49038c168e5cc2955b4517133e57a141fc33
Ainamkoz Bakytkaliyeva <ainamkoz.bakyt@gmail.com>
2021-02-16 22:49:22 +0100

    deleteButton

--- file.js
+++ file.js
@@ -4,12 +4,16 @@ const buttonAdd = document.querySelector("#add");
 const incomplete = document.querySelector('#incomplete'); 
 
 
-// Event Listener
-
+// Event Listeners
 buttonAdd.addEventListener("click", addToDo); 
+incomplete.addEventListener("click", deleteDoneButton); 
+
+
+// Functions
 
 function addToDo(event) {
     event.preventDefault(); 
+
 // Create a div for toDo items 
     const incompleteList = document.createElement('div');
     incompleteList.classList.add('toDo'); 
@@ -31,6 +35,22 @@ function addToDo(event) {
     deleteButton.classList.add('delete-button')
     incompleteList.appendChild(deleteButton)
     incomplete.appendChild(incompleteList);
+
+    // clear Input value after adding new ToDo item 
+    toDoInput.value = "";
+}
+
+// Delete Function
+function deleteDoneButton(e) {
+    const item = e.target; 
+    if(item.classList[0] === 'delete-button') {
+        const toDo = item.parentElement; 
+        toDo.remove();
+    }
+    if(item.classList[0] === 'done-button') {
+        const toDo = item.parentElement; 
+        toDo.classList.toggle("done");
+    }
 }
 
 
