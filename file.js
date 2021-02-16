a59c8247a9fd434a2e46ed0a8f45c0c1341038ae
Ainamkoz Bakytkaliyeva <ainamkoz.bakyt@gmail.com>
2021-02-16 16:43:38 +0100

    add to do list items

--- file.js
+++ file.js
@@ -1,23 +1,58 @@
-const submitForm = document.getElementById("inputform");
-const userInput = document.getElementById("new-task");
+const userInput = document.getElementById("newToDo");
 const deleteButton = document.getElementById("delete");
+const form = document.querySelector("form");
+const listSelection = document.querySelector("#incomplete-tasks")
+const deleteIcon =  ["fa-trash"]; 
 
 
-class Todo (task) {
-    constructor (){
-        this.inputTask = input
+class ToDo {
+    constructor () {
+    this.inputTask = input  
     }
+} 
+
+// to create a list of ToDO items
+
+const toDoItems = [];
 
 
-    function addTask () {
-        const newTask = document.createElement("tagName[, options]");
-    }
+function renderToDoItem(toDo) {
+    const isIncomplete = toDo.Incomplete ? 'completed' : '';
+    const node = document.createElement("li");
+    node.setAttribute('class', `toDo-item ${isIncomplete}`)
+    node.setAttribute('data-key', toDo.id);
+    node.innerHTML = `
+      <input id="${toDo.id}" type="checkbox" type="text"/>
+      <label for="${toDo.id}" class="tick js-tick"></label>
+      <span>${toDo.text}</span>
+      <button class="delete-todo js-delete-todo">
+      <use> <i class="fal fa-trash-alt"></i> </use>
+      </button>
+    `;
 
-    let task = (event) => {
-        event.preventDefault();
-        if (userInput.value === "") {
-          alert("Empty");
-        } else {
-          addTask
-        }
+    listSelection.append(node);
+
 }
+
+
+function addToDoItem(text) {
+    const toDo = {
+        text, 
+        checked: false, 
+        id: Date.now(),
+    }; 
+    toDoItems.push(toDo); 
+    renderToDoItem(toDo);
+    }
+
+   form.addEventListener("submit", event => {
+       event.preventDefault(); 
+       const text = userInput.value.trim();
+       if (text !== '') {
+           addToDoItem(text);
+           userInput.value = '';
+           userInput.focus(); 
+       }
+   });
+
+
