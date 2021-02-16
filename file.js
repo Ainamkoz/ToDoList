09ad9b9b07a6f04e7a3f4791441929bff2fec18b
agnes zimmer <agnes.m.zimmer@gmail.com>
2021-02-16 18:02:40 +0100

    shit

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 const userInput = document.getElementById("newToDo");
 const deleteButton = document.getElementById("delete");
 const form = document.querySelector("form");
-const listSelection = document.querySelector("#incomplete-tasks")
+const incompleteTast = document.querySelector("#incomplete-tasks")
 const deleteIcon =  ["fa-trash"]; 
 
 
@@ -15,13 +15,12 @@ class ToDo {
 
 const toDoItems = [];
 
-
 function renderToDoItem(toDo) {
     const isIncomplete = toDo.Incomplete ? 'completed' : '';
-    const node = document.createElement("li");
-    node.setAttribute('class', `toDo-item ${isIncomplete}`)
-    node.setAttribute('data-key', toDo.id);
-    node.innerHTML = `
+    const task = document.createElement("li");
+    task.setAttribute('class', `toDo-item ${isIncomplete}`)
+    task.setAttribute('data-key', toDo.id);
+    task.innerHTML = `
       <input id="${toDo.id}" type="checkbox" type="text"/>
       <label for="${toDo.id}" class="tick js-tick"></label>
       <span>${toDo.text}</span>
@@ -30,28 +29,32 @@ function renderToDoItem(toDo) {
       </button>
     `;
 
-    listSelection.append(node);
-
+    incompleteTask.append(task);
 }
-
 
 function addToDoItem(text) {
     const toDo = {
         text, 
-        checked: false, 
+        checked: false,    //?
         id: Date.now(),
     }; 
     toDoItems.push(toDo); 
     renderToDoItem(toDo);
     }
 
-   form.addEventListener("submit", event => {
+function moveItem(){
+  
+}
+
+function deleteItem
+
+form.addEventListener("submit", event => {
        event.preventDefault(); 
        const text = userInput.value.trim();
        if (text !== '') {
            addToDoItem(text);
            userInput.value = '';
-           userInput.focus(); 
+ //  ?     userInput.focus(); 
        }
    });
 
